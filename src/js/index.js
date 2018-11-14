95e2447522c031a34541bed87c875e3ff0e49851
José Carlos <carlos.avellar@gmail.com>
2018-11-14 15:34:00 -0200

    Search in Recipe Obj + Click

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,23 @@
-const name = 'José';// Global app controller
+import Search from './models/Search';
+
+const result = new Search("pizza");
+
+result.getResults();
 
 
-import perigo from './test';
-alert(`${perigo()}`);
+const state = {};
+const controllSeach = () =>{
+    const query = 'tomato';
+    if (query) {
+        try{    
+            state.search = new Search(query);
+            state.search.getResults();
+        }catch(err){
+            console.log(err);
+        }
+    }
+}
+document.querySelector('.search').addEventListener("submit", e=>{
+    e.preventDefault();
+    controllSeach();
+});
---
+++ src/js/views/searchView.js
@@ -0,0 +1,3 @@
+// 68a8b8bf670780455d6b07de135e89f0
+// https: //www.food2fork.com/api/search
+// https://www.food2fork.com/api/get
