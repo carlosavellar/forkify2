205a2ae65576c143899e426b719b81a24fe04298
José Carlos <carlos.avellar@gmail.com>
2018-11-15 12:44:04 -0200

    Algorithmn  - Limit Character

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -5,6 +5,22 @@ import { elements } from './base';
 
 export const getInput = () => elements.inputS.value;
 
+
+const limitCharacter = (title, limit = 17) => {
+    const newTtitle = [];
+    if(title.length > limit){
+        title.split(' ').reduce((acc, curr)=>{
+            if(acc + curr.length <= limit){
+                newTtitle.push(curr);
+            }
+            return acc + curr.length;
+        }, 0);
+        return `${newTtitle.join(' ')},...`;
+
+    }
+    return title;
+}
+
 const renderRecipe = recipe => {
     
     const markup = `<li>
@@ -13,7 +29,7 @@ const renderRecipe = recipe => {
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitCharacter(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}/p>
                         </div>
                     </a>
@@ -23,7 +39,7 @@ const renderRecipe = recipe => {
 }
 
 export const renderResult = recipes => {
-    debugger;
+    // debugger;
     window.r = recipes;
     recipes.forEach(renderRecipe);
 }
